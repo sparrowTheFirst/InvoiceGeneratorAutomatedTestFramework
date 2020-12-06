171dd2d5ee3a0ec71a782e16fa22686e4c4ef457
Marcin Wróbel <wrobel.sparrow@gmail.com>
2020-12-06 11:51:00 +0100

    Selenium WebDriver, WebDriverManager libs and WebDriverSetup class added

--- src/main/java/steps/LoginSteps.java
+++ src/main/java/steps/LoginSteps.java
@@ -4,11 +4,13 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
+import static steps.WebDriverSetup.*;
+
 public class LoginSteps {
 
     @Given("User is on login page")
     public void userIsOnLoginPage() {
-
+        driver.get("localhost:8081");
     }
 
     @When("User enters user name and password")
---
+++ src/main/java/steps/WebDriverSetup.java
@@ -0,0 +1,25 @@
+package steps;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WebDriverSetup {
+
+    public static WebDriver driver;
+
+    @Before
+    public void setup() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+    }
+
+    @After
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
