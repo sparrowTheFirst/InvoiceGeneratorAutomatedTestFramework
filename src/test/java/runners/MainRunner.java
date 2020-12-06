804779f39180d6b8ff9dfbb66837d2c2c4dae65c
Marcin Wróbel <wrobel.sparrow@gmail.com>
2020-12-06 11:23:42 +0100

    Cucumber reporting lib and MainRunner added

---
+++ src/test/java/runners/MainRunner.java
@@ -0,0 +1,14 @@
+package runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/java/features",
+        glue = "steps",
+        plugin = { "de.monochromata.cucumber.report.PrettyReports:target/cucumber" })
+public class MainRunner {
+
+}
